18a220ddcbd2571f9fc25e1276acf2d93017b445
shiwa786 <74191978+shiwa786@users.noreply.github.com>
2022-10-11 11:57:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,6 +6,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5545963604:AAFyeZDxieUOq1mmz6khwuSzTvvf8JiBJcU")
     STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOIkBuw7FSpkcGqW7gL7CmA02Zls38Az-XIHggV_l1z1iOm-dror57c_Dp_YnnS_liJ-8MaYkt3MFLnTWFFZweFLnRH5MGa70T29UX-hw9TQXTPiES--fn06xUaNMTtE9APBn7zwllfDhVJSTsS_wPMAUpKjpgIrMymA_Hzv0Yn6haXBrLABzJVNiAmRfRwsfNTTWxGz0uxrvbAuiuqxcqyb9WRGq6WGrGB1ePxjHLEA3UcrpCRM4_kxb2BrkuGGGacu8AVGjlLV87SU3VMp8KP97Y_cnk-YQmkQdjwIQaBeaVAtc7oFi6TavwDR66DbGaO5AKV9M2hHUVs7zUnMLaC2MyEw=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", "ENABLE")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "miselisarobot")
     SUPPORT = os.environ.get("SUPPORT", "elisha_support") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "denvil_bots") # Your Channel
